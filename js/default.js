a135610b02f27401e48b78074fa30af091550a8a
jie lian <imjeee@gmail.com>
2012-04-26 00:33:00 -0500

    factored out javascript code

--- js/default.js
+++ js/default.js
@@ -1,3 +1,34 @@
-<script type="text/javascript" charset="utf-8">
-  $(function(){ $("label").inFieldLabels(); });
-</script>
+$(function(){
+
+  $("label").inFieldLabels(); // 1.html specific
+
+  // 2.html specific
+  $(".typeOfCare").hide();
+  
+  $("#radio1").click(function() {
+    $(".typeOfCare").show("fast");
+  });
+
+  $("#radio2").click(function() {
+    $(".typeOfCare").hide("fast");
+  });
+
+  $(".radio").buttonset();
+
+  $(".ui-button").hover(
+    function(){ 
+      $(this).addClass("ui-state-hover"); },
+    function(){ $(this).removeClass("ui-state-hover");
+              });
+  
+  // 4, 5.html specific
+  $(".eval").click(function() {
+    $("dialog:ui-dialog").dialog("destroy");
+    $("#dialog-modal").dialog({ 
+      height: 300, 
+      modal: true,
+      width: 600
+    });
+  });
+
+});
